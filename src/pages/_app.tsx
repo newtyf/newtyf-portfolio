506a23ea5228006bd14a7cb97342e287c077bdcc
newt <axelskam.123@outlook.es>
2023-04-15 14:59:20 -0500

    init project and setup first home screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+// import 'bootstrap/dist/css/bootstrap.css'
+import "animate.css"
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Newt Yf</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
