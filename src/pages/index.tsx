96f3491ed4a011300c7d6f1eb4a90764a4642b03
newt <axelskam.123@outlook.es>
2023-04-15 16:10:12 -0500

    format code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
         <nav>
           <ol>
             <li>
-              <Link className="active" href={"/"}>About</Link>
+              <Link className='active' href={"/"}>
+                About
+              </Link>
             </li>
             <li>
               <Link href={"/"}>Work</Link>
@@ -26,13 +28,17 @@ export default function Home() {
       </header>
       <main>
         <div className='home'>
-          <h1 className="title-home animate__animated animate__fadeInDown" >YoungFlex</h1>
-          <p className="animate__animated animate__fadeInDown">
+          <h1 className='title-home animate__animated animate__fadeInDown'>
+            YoungFlex
+          </h1>
+          <p className='animate__animated animate__fadeInDown'>
             {
               "I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm searching new projects to participate and create amazing digital experience."
             }
           </p>
-          <button className="animate__animated animate__fadeInDown">Contact me!</button>
+          <button className='animate__animated animate__fadeInDown'>
+            Contact me!
+          </button>
         </div>
       </main>
     </>
