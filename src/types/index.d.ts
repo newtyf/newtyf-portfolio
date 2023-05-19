2f48600bd2cadb1e92172326bf8ec81d61d98283
newt <axelskam.123@outlook.es>
2023-05-19 11:15:25 -0500

    fix gtag type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,31 +3,31 @@ declare global {
     particlesJS: {
       load: (value: string, optionsPath: string, onload: Function) => {};
     };
+    gtag: any;
   }
 }
 
 type option = {
   title: string;
   link: string;
-  styleCustom?: boolean
+  styleCustom?: boolean;
 };
 
 type job = {
-  title: string,
-  description: string[],
-  role: string,
-  start: string,
-  end: string
-}
+  title: string;
+  description: string[];
+  role: string;
+  start: string;
+  end: string;
+};
 
 type project = {
-  title: string,
-  description: string,
-  tags: string[],
-  image: string,
-  github: string,
-  liveSite?: string
-}
+  title: string;
+  description: string;
+  tags: string[];
+  image: string;
+  github: string;
+  liveSite?: string;
+};
 
-
-export {option, job, project};
+export { option, job, project };
